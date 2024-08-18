2deccd024d4a6ffc4fd1cf59a085be0b857d9c73
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-08-18 17:06:51 +0200

    Added the API key

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "6b8a7398o06094tf908737c5b44dce83";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+}
+
 function handelSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
