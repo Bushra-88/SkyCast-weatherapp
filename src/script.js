7f6f97560e28f3909c4f32c77a9fcdb5daa5c51b
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-08-18 20:11:09 +0200

    Create the icon based on the api

--- src/script.js
+++ src/script.js
@@ -1,17 +1,23 @@
 function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let city = response.data.city;
+
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
   let descriptionElement = document.querySelector("#description");
   let description = response.data.condition.description;
+
   let humidityElement = document.querySelector("#humidity");
   let humidity = `${response.data.temperature.humidity}%`;
+
   let windSpeedElement = document.querySelector("#wind-speed");
   let windSpeed = `${response.data.wind.speed} km/h`;
+
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = city;
   timeElement.innerHTML = formatDate(date);
@@ -19,6 +25,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = humidity;
   windSpeedElement.innerHTML = windSpeed;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
