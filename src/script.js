3fd03835463c51d097ee054fd4a40fd79acc6453
Bushra Alhusaini <alhusaini.b2023@gmail.com>
2024-08-18 18:43:21 +0200

    Get the humidity and the wind speed

--- src/script.js
+++ src/script.js
@@ -2,12 +2,19 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let city = response.data.city;
   let descriptionElement = document.querySelector("#description");
   let description = response.data.condition.description;
-
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = `${response.data.temperature.humidity}%`;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let windSpeed = `${response.data.wind.speed} km/h`;
+  console.log(response.data);
   temperatureElement.innerHTML = Math.round(temperature);
-  cityElement.innerHTML = response.data.city;
+  cityElement.innerHTML = city;
   descriptionElement.innerHTML = description;
+  humidityElement.innerHTML = humidity;
+  windSpeedElement.innerHTML = windSpeed;
 }
 
 function searchCity(city) {
